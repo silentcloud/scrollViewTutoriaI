cf37dace468349137403ebf95a03e782dccea00d
竹卢 <qiang.mou@alipay.com>
2013-11-02 16:58:22 +0800

    Initial Commit

---
+++ ScrollViewTest/ScrollViewTestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ScrollViewTestAppDelegate.h
+//  ScrollViewTest
+//
+//  Created by silentcloud on 11/2/13.
+//  Copyright (c) 2013 qiang.mou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollViewTestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrollViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScrollViewTest
+//
+//  Created by silentcloud on 11/2/13.
+//  Copyright (c) 2013 qiang.mou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ScrollViewTestAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ScrollViewTestAppDelegate class]));
+    }
+}
